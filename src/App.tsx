d4f7b5447de5d4ce1e4eb92ac643d26b481f5907
EDUARDO ALVES MARTINS NETO <emailforprojects01@gmail.com>
2023-12-12 13:41:49 -0300

    remove: remoção do eslint

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,15 @@ import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./routes";
 import { LigthTheme } from "./shared/themes";
 import { AppThemeProvider } from "./shared/contexts/ThemeContext";
+import { MenuDrawer } from "./shared/components";
 
 export const App = () => {
   return (
-      <AppThemeProvider> 
-            <BrowserRouter>
-        <AppRoutes/>
-        </BrowserRouter>
-      </AppThemeProvider>
+    <AppThemeProvider>
+      <BrowserRouter>
+        <MenuDrawer />
+        <AppRoutes />
+      </BrowserRouter>
+    </AppThemeProvider>
   );
-       
 };
---
+++ src/shared/components/menu-drawer/MenuDrawer.tsx
@@ -0,0 +1,12 @@
+import { Drawer } from "@mui/material";
+import React from "react";
+
+export const MenuDrawer : React.FC = () =>{
+
+    return(
+        <Drawer variant="permanent">
+            teste
+        </Drawer>
+    );
+
+}
